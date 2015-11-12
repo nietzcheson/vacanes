765d5e36299abbf346d3fe570bc49c3801238ea8
Cristian Angulo Nova <cristianangulonova@gmail.com>
2015-11-11 21:16:48 -0500

    AuthController

--- src/AppBundle/Controller/AuthController.php
+++ src/AppBundle/Controller/AuthController.php
@@ -3,125 +3,113 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Controller\TokenAuthenticatedController;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Entity\User;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-use AppBundle\Entity\User;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AuthController extends Controller //implements TokenAuthenticatedController
 {
 
-    public function usersAction(Request $request)
+    public function userAction($id, Request $request)
     {
-
-        $em = $this->getDoctrine()->getManager();
         $response = new Response();
         $serializer = $this->get('serializer');
 
-        switch ($request->getMethod()) {
-            case 'GET':
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository('AppBundle:User')->findOneBy(array('facebookId' => $id));
 
-                /*
-                 * Call Object
-                 */
-
-                $user = $em->getRepository('AppBundle:User')->findOneBy(array('facebookId' => $request->query->get('facebookId')));
-
-                if(!$user){
-                    $response->setStatusCode(204, 'No user found');
-                }
-
-                $response->setContent($serializer->serialize($user, 'json', array('groups' => array('user'))));
-
-                break;
-            case 'POST':
-
-                /*
-                 * Create Object
-                 */
-
-                $user = new User();
-                $user->setFacebookId($request->request->get('facebookId'));
-                $user->setFirstName($request->request->get('firstName'));
-                $user->setLastName($request->request->get('lastName'));
-                $user->setEmail($request->request->get('email'));
-                $user->setToken($request->request->get('token'));
-
-                $validator = $this->get('validator');
-                $errors = $validator->validate($user);
-
-                if (count($errors) > 0) {
-
-                    $errorsString = (string) $errors;
-
-                    $response->setStatusCode(409, 'Errors');
-
-                    $response->setContent($serializer->serialize($errorsString, 'json'));
-
-                } else {
-                    $em->persist($user);
-                    $em->flush();
-
-                    $response->setContent($serializer->serialize('CREATED', 'json'));
-                }
-
-
-                break;
-            case 'PUT':
-
-                /*
-                 * Update Object
-                 */
-
-                $user = $em->getRepository('AppBundle:User')->findOneBy(array('facebookId' => $request->request->get('facebookId')));
-
-                $user->setFirstName($request->request->get('firstName'));
-                $user->setLastName($request->request->get('lastName'));
-                $user->setEmail($request->request->get('email'));
-                $user->setToken($request->request->get('token'));
-
-                $validator = $this->get('validator');
-                $errors = $validator->validate($user);
-
-                if (count($errors) > 0) {
-
-                    $errorsString = (string) $errors;
-
-                    $response->setStatusCode(409, 'Errors');
-
-                    $response->setContent($serializer->serialize($errorsString, 'json'));
-
-                } else {
-                    $em->flush();
-
-                    $response->setContent($serializer->serialize('UPDATED', 'json'));
-                }
-
-                break;
-            case 'DELETE':
-
-                /*
-                 * Delete Object
-                 */
-
-                $user = $em->getRepository('AppBundle:User')->findOneBy(array('id' => $request->request->get('id')));
-
-                if(!$user){
-                    $response->setStatusCode(204, 'No user found');
-                }
-
-                $em->remove($user);
-                $em->flush($user);
-
-                $response->setContent($serializer->serialize('DELETE', 'json'));
-                break;
-            default:
-                $response->setContent($serializer->serialize('Method not expected: '.$request->getMethod(), 'json'));
-                break;
+        if(!$user){
+            $response->setStatusCode(204, 'No user found');
         }
 
-        return $response;
+        return new Response($this->get('serializer')->serialize($user, 'json', array('groups' => array('user'))));
+    }
+
+    public function userCreateAction(Request $request)
+    {
+        $response = new Response();
+        $serializer = $this->get('serializer');
+        $em = $this->getDoctrine()->getManager();
+
+        $user = new User();
+
+        $user->setFacebookId($request->request->get('facebookId'));
+        $user->setFirstName($request->request->get('firstName'));
+        $user->setLastName($request->request->get('lastName'));
+        $user->setEmail($request->request->get('email'));
+        $user->setToken($request->request->get('token'));
+
+        $validator = $this->get('validator');
+        $errors = $validator->validate($user);
+
+        if (count($errors) > 0) {
+
+            $errorsString = (string) $errors;
+            $response->setStatusCode(409, 'Errors');
+
+            return $response->setContent($serializer->serialize($errorsString, 'json'));
+        }
+
+        $em->persist($user);
+        $em->flush();
+
+        return $response->setContent($serializer->serialize('CREATED', 'json'));
+    }
+
+    public function userUpdateAction(Request $request)
+    {
+        $response = new Response();
+        $em = $this->getDoctrine()->getManager();
+        $serializer = $this->get('serializer');
+
+        $user = $em->getRepository('AppBundle:User')->findOneBy(array('id' => $request->request->get('id')));
+
+        if(!$user){
+            $response->setStatusCode(204, 'No user found');
+        }
+
+        $user->setFirstName($request->request->get('firstName'));
+        $user->setLastName($request->request->get('lastName'));
+        $user->setEmail($request->request->get('email'));
+        $user->setToken($request->request->get('token'));
+
+        $validator = $this->get('validator');
+        $errors = $validator->validate($user);
+
+        if (count($errors) > 0) {
+
+            $errorsString = (string) $errors;
+
+            $response->setStatusCode(409, 'Errors');
+
+            return $response->setContent($serializer->serialize($errorsString, 'json'));
+        }
+
+        $em->flush();
+
+        return $response->setContent($serializer->serialize('UPDATED', 'json'));
+    }
+
+    public function userDeleteAction(Request $request)
+    {
+        $response = new Response();
+        $serializer = $this->get('serializer');
+
+        $em = $this->getDoctrine()->getManager();
+
+        $user = $em->getRepository('AppBundle:User')->findOneBy(array('id' => $request->request->get('id')));
+
+        if(!$user){
+            $response->setStatusCode(204, 'No user found');
+            return $response->setContent($serializer->serialize($user, 'json'));
+        }
+
+        $em->remove($user);
+        $em->flush($user);
+
+        return $response->setContent($serializer->serialize('DELETE', 'json'));
     }
 
 }
